4ab632ce02e799fec70b7a16826f830e894ebf15
tduclos <67517520+tduclos@users.noreply.github.com>
2020-07-22 12:58:12 -0400

    up through step 4

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,16 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game{
+    constructor(){
+        this.missed = 0;
+        this.phrases = [
+            {phrase: 'the cake is a lie'},
+            {phrase: 'its a secret to everybody'},
+            {phrase: 'i am error'},
+            {phrase: 'your princess is in another castle'},
+            {phrase: 'a slime draws near'}];
+        this.activePhrase = null;
+    }    
+};
